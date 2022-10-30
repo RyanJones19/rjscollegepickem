2f1d7030890994ad13dc783b045935fcb05e9e0f
jonesr <ryan.a.jones@autodesk.com>
2022-10-29 19:23:46 -0700

    Add sql

---
+++ project/sql/adminselections.sql
@@ -0,0 +1,16 @@
+CREATE TABLE adminselections (
+    year int NOT NULL,
+    week1 VARCHAR(255),
+    week2 VARCHAR(255),
+    week3 VARCHAR(255),
+    week4 VARCHAR(255),
+    week5 VARCHAR(255),
+    week6 VARCHAR(255),
+    week7 VARCHAR(255),
+    week8 VARCHAR(255),
+    week9 VARCHAR(255),
+    week10 VARCHAR(255),
+    week11 VARCHAR(255),
+    week12 VARCHAR(255),
+    PRIMARY KEY (year)
+);
---
+++ project/sql/user.sql
@@ -0,0 +1,8 @@
+CREATE TABLE user (
+    id int NOT NULL AUTO_INCREMENT,
+    email varchar(100) NOT NULL,
+    name varchar(1000) NOT NULL,
+    password varchar(100) NOT NULL,
+    admin int NOT NULL,
+    PRIMARY KEY (id)
+);
